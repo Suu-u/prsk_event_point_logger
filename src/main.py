72a2ccd7850a1b544a1c04b29efa7653ab13a94a
Suu__u <50479126+Suu-u@users.noreply.github.com>
2024-03-08 19:14:34 +0900

    Add life row

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import datetime
 from os import path
 
 POINT_FILEPATH = "point.csv"
-POINT_HEADER = ["date", "point", "comment"]
+POINT_HEADER = ["date", "point", "life", "comment"]
 BORDER_FILEPATH = "border.csv"
 BORDER_HEADER = ["date", "point"]
 
@@ -13,20 +13,38 @@ def main():
     create_file(POINT_FILEPATH, POINT_HEADER)
     create_file(BORDER_FILEPATH, BORDER_HEADER)
 
+    life = 5
     while True:
         output_list = []
         input_str = input(">>> ")
         if input_str.isdigit():
             output_list.append(datetime.datetime.now().isoformat())
             output_list.append(int(input_str))
+            output_list.append(life)
             append_line(POINT_FILEPATH, output_list)
         else:
             if input_str == "comment":
                 comment = input(">>> [comment] ")
                 output_list.append(datetime.datetime.now().isoformat())
                 output_list.append("NA")
+                output_list.append("NA")
                 output_list.append(comment)
                 append_line(POINT_FILEPATH, output_list)
+            elif input_str == "life":
+                while  True:
+                    new_life = input(">>> [life] ")
+                    if new_life == "cancel":
+                        break
+                    if not new_life.isdigit():
+                        print("[ERROR] Inbalid input: Input life number or 'cancel' to exit life mode")
+                    else:
+                        new_life = int(new_life)
+                        if new_life <= 0 or new_life > 10:
+                            print("[ERROR] Inbalid input: Life number is 1 or more and 10 or less")
+                        else:
+                            print(f"Set life number to {new_life}")
+                            life = new_life
+                            break
             elif input_str == "border":
                 while  True:
                     border = input(">>> [border] ")
@@ -46,7 +64,7 @@ def main():
                 return
             else:
                 print("[ERROR] Invalid input: Input point number or following command")
-                print("        'comment', 'border', 'end'")
+                print("        'comment', 'life', 'border', 'end'")
 
 
 def sum_points(filepath):
@@ -62,6 +80,7 @@ def sum_points(filepath):
             return sum
     except Exception as e:
         raise Exception(e)
+
 
 def create_file(filepath,headers_list):
     if path.exists(filepath):
