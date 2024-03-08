02dbe1ff2d9a03107aab3e3f3cddb490c57d0150
Suu__u <50479126+Suu-u@users.noreply.github.com>
2024-03-08 18:19:28 +0900

    Add point logging

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 from os import path
 
 POINT_FILEPATH = "point.csv"
@@ -11,6 +12,14 @@ def main():
     print("Checking fine existence...")
     create_file(POINT_FILEPATH, POINT_HEADER)
     create_file(BORDER_FILEPATH, BORDER_HEADER)
+
+    while True:
+        output_list = []
+        input_list = input(">>> ").split(" ")
+        if input_list[0].isdigit():
+            output_list.append(datetime.datetime.now().isoformat())
+            output_list.append(int(input_list[0]))
+            append_line(POINT_FILEPATH, output_list)
 
 
 def create_file(filepath,headers_list):
@@ -32,6 +41,7 @@ def append_line(filepath, line):
         with open(filepath, "a", encoding="utf-8", newline="") as file_object:
             writer = csv.writer(file_object)
             writer.writerow(line)
+            print(f"Write {line}")
         return
     except Exception as e:
         raise Exception(e)
